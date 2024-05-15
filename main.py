9f1d747f1123b02af701448ca8b46988bf4247da
HU KE <403392669@qq.com>
2024-05-15 14:58:17 +0800

    update

---
+++ main.py
@@ -0,0 +1,16 @@
+# print('hello world')
+import os
+from dotenv import load_dotenv
+
+import openai
+import langchain
+from langchain.llms import OpenAI
+
+load_dotenv()
+
+my_key = os.getenv('OPENAI_API_KEY')
+print(f"Key is: {my_key}")
+
+llm = OpenAI(temperature=0, openai_api_key=my_key)
+text = "What is AI?"
+print(llm(text))

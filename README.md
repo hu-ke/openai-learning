a918e6cddf3df0aa49859f5bde20a3b9498813b1
HU KE <403392669@qq.com>
2024-05-16 16:07:18 +0800

    feat: langserve fastapi例子

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ pip install --upgrade pip
 pip install -r requirement.txt
 
 Open the Command Palette (Ctrl+Shift+P), start typing the Python: Create Environment
+
+# 案例例子：
+* https://www.youtube.com/watch?v=FgTYqJVNdkU
+* https://github.com/krishnaik06/Langserve-Demo
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# print('hello world')
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langserve import add_routes
+import uvicorn
 import os
 from dotenv import load_dotenv
-
-import openai
-import langchain
-from langchain_openai import OpenAI
-
 load_dotenv()
 
-my_key = os.getenv('OPENAI_API_KEY')
-print(f"Key is: {my_key}")
+print(os.getenv("OPENAI_API_KEY"))
 
-llm = OpenAI(temperature=0, openai_api_key=my_key)
-text = "What is AI?"
-print(llm(text))
+app=FastAPI(
+    title="Langchain Server",
+    version="1.0",
+    decsription="A simple API Server"
+
+)
+
+add_routes(
+    app,
+    ChatOpenAI(),
+    path="/openai"
+)
+
+model=ChatOpenAI()
+prompt=ChatPromptTemplate.from_template("provide me an essay about {topic}")
+prompt1=ChatPromptTemplate.from_template("provide me a poem about {topic}")
+
+add_routes(
+    app,
+    prompt|model,
+    path="/essay"
+
+)
+
+add_routes(
+    app,
+    prompt1|model,
+    path="/poem"
+
+)
+
+if __name__=="__main__":
+    uvicorn.run(app,host="localhost",port=8000)

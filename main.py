592312597a8ee60ebcfaca574c8bad606d9d1088
HU KE <403392669@qq.com>
2024-05-22 10:19:40 +0800

    feat: 可以上传文件到python fastapi后台

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# print('hello world')
-import os
-from dotenv import load_dotenv
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from pathlib import Path
+UPLOAD_DIR = Path() / 'upload'
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
-import openai
-import langchain
-from langchain_openai import OpenAI
+app = FastAPI()
 
-load_dotenv()
+origins = [
+    "http://localhost:3000",
+]
 
-my_key = os.getenv('OPENAI_API_KEY')
-print(f"Key is: {my_key}")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-llm = OpenAI(temperature=0, openai_api_key=my_key)
-text = "What is AI?"
-print(llm(text))
+@app.post("/uploadfile/")
+async def create_upload_file(file_upload: UploadFile):
+    data = await file_upload.read()
+    save_to = UPLOAD_DIR / file_upload.filename
+    with open(save_to, 'wb') as f:
+        f.write(data)
+    return {"filename": file_upload.filename}
+
+@app.get('/')
+async def root(): 
+    return { 'message': 'Hello World'}
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
